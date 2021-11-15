10b3d4e30579f2fd1a1f518785d1b6837a2a94e9
Mohd Bilal <69635539+bilal8533@users.noreply.github.com>
2021-11-15 12:18:02 +0530

    Create Apisetucowin.js

---
+++ Apisetucowin.js
@@ -0,0 +1,21 @@
+var http= require('http');
+
+var server= http.createServer(function (req,res))
+{
+var request=require('request');
+request(https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByCenter?center_id=201206&date=15-11-2021),function(error,responding,body))
+{
+    var data=JSON.parse(body);
+    //res.write(body);
+    let text="";
+    for(let i=o;i<data.session[i].name+"  ; " +data.sessions[i].available_capacity_dose1+" , " ;   }
+
+}
+res.write(test);
+res.end('');
+    ));
+    )).sessions
+
+
+
+}
